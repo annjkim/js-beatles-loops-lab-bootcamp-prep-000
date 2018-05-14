fc9a1a1c7ac79126a8168633217d0d43b16f5d3d
annkim822@gmail.com Kim <annkim822@gmail.com>
2018-05-14 20:40:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,14 +6,13 @@ function theBeatlesPlay(musicians, instruments){
   return newMusicians;
 }
 
-  const facts = [
+function johnLennonFacts(){
+  var facts = [
   "He was the last Beatle to learn to drive",
   "He was never a vegetarian",
   "He was a choir boy and boy scout",
   "He hated the sound of his own voice"
   ];
-  
-function johnLennonFacts(){
   var newFacts = [];
   var i = 0;
   while (i < facts.length) {
