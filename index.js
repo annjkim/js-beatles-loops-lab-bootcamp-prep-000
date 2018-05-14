e956d108c307085f60f800efc583242d8ee72394
annkim822@gmail.com Kim <annkim822@gmail.com>
2018-05-14 20:40:10 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,24 @@
+function theBeatlesPlay(musicians, instruments){
+  var newMusicians = [];
+  for (var i=0; i < musicians.length; i++){
+    newMusicians.push(musicians[i] + " plays " + instruments[i])
+  }
+  return newMusicians;
+}
+
+  const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+  ];
+  
+function johnLennonFacts(){
+  var newFacts = [];
+  var i = 0;
+  while (i < facts.length) {
+    newFacts.push(facts[i] + "!!!");
+    i++;
+  }
+  return newFacts;
+}
